11056316a71bedf502aabddd19934fc2302dbf57
Abdullah <abdullah.cibik@icloud.com>
2025-08-03 09:30:03 +0200

    socket update

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // Kendi bilgisayarınızın IP adresini yazın.
-const SOCKET_URL = 'http://192.168.1.29:3000';
+const SOCKET_URL = 'https://isimsehiroyunu.onrender.com';
 
 // Uygulama boyunca kullanılacak tek bir socket bağlantısı oluşturup dışa aktarıyoruz.
 export const socket = io(SOCKET_URL, {
